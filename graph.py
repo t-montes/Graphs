2787c1075432212290fbee83dd00658264a41442
Tony Santiago Montes Buitrago <t.montes@uniandes.edu.co>
2021-11-05 18:40:34 -0500

    modify graph class

--- graph.py
+++ graph.py
@@ -18,47 +18,51 @@ def timer(func):
     return inner
 
 class Graph():
-    def __init__(self, nodes:list, edges:list, adjMatrex: list):
-        self.n = nodes
+    def __init__(self, vertices:list, edges:list, adjMatrix: list):
+        self.v = vertices
         self.e = edges
-        self.m = adjMatrex
+        self.m = adjMatrix
     
-    """Dijkstra minimum cost algorithm"""
-    @timer
+    def minimum_cost(self,algorithm="dijkstra"):
+        if algorithm == "dijkstra":
+            return self.all_dijkstra()
+        elif algorithm == "bellman_ford":
+            return self.all_bellman_ford()
+        else:
+            return self.floyd_warschall()
+
     def dijkstra(self,font):
+        """Dijkstra minimum cost algorithm"""
         ...
     
+    @timer
     def all_dijkstra(self):
         mins:list = []
         for i in self.v:
             mins.append(self.dijkstra(i))
         return mins
     
-    """Bellman Ford minimum cost algorithm"""
-    @timer
     def bellman_ford(self,font):
-        costs:list = [float("Inf")]*len(self.n)
-        matrix:list = self.m
-
+        """Bellman Ford minimum cost algorithm"""
+        costs:list = [float("inf")]*len(self.v)
         costs[font] = 0
 
-        for i in range(len(self.n)-1):
+        for i in range(len(self.v)-1):
             for edge in self.e:
                 costs[edge[1]] = min(costs[edge[1]],costs[edge[0]]+edge[2])
         return costs
 
-
-    @timer  
+    @timer
     def all_bellman_ford(self):
         mins:list = []
-        for i in self.n:
+        for i in self.v:
             mins.append(self.bellman_ford(i))
         return mins
     
-    """Floyd Warschall minimum cost algorithm"""
     @timer
     def floyd_warschall(self):
-        n = len(self.n)
+        """Floyd Warschall minimum cost algorithm"""
+        n = len(self.v)
         m = [[[0 for x in range(n)] for y in range(n)]for k in range(n+1)]
         k = 0
         
@@ -79,7 +83,7 @@ class Graph():
             k+=1
         
         return m[n]
-    
+
     """Depth First Search algorithm"""
     @timer
     def dfs(self,kind=1):
@@ -95,10 +99,10 @@ def loadGraph(path:str, first='A') -> Graph:
     with open(path,'r') as file:
         lines:list = file.readlines()
     
-    numbernodes:int = len(lines)
+    numbervertices:int = len(lines)
 
-    Vertex:list = [i for i in range(first,first+numbernodes)] if (type(first) is int) else [
-                chr(i) for i in range(ord(first),ord(first)+numbernodes)]
+    Vertices:list = [i for i in range(first,first+numbervertices)] if (type(first) is int) else [
+                chr(i) for i in range(ord(first),ord(first)+numbervertices)]
     Edges:list = []
 
     Matrix: list = []
@@ -111,8 +115,8 @@ def loadGraph(path:str, first='A') -> Graph:
         while (j := j+1) < len(subline):
             if subline[j] > 0:
                 #Tripla A -> B de costo C
-                Edges.append((Vertex[i],Vertex[j],subline[j]))
-    return Graph(Vertex,Edges, Matrix)
+                Edges.append((Vertices[i],Vertices[j],subline[j]))
+    return Graph(Vertices,Edges, Matrix)
 
 if __name__ == "__main__":
     main.main()
--- main.py
+++ main.py
@@ -5,10 +5,24 @@
 
 import graph
 
+printl = print
+def print(*args,**kwargs):
+    printl(*args,**kwargs)
+    printl("-"*50)
+
 path:str = "Data/distances5.txt"
 
 def main():
     g:graph.Graph = graph.loadGraph(path,first=0)
+    
+    cs1 = g.minimum_cost("dijkstra")
+    cs2 = g.minimum_cost("bellman_ford")
+    cs3 = g.minimum_cost("floyd_warschall")
+
+    print(cs1)
+    print(cs2)
+    print(cs3)
+
 
 if __name__ == "__main__":
     main()
